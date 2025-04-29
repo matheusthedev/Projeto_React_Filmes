f9e70e5f913e88152cad0ebfac5da3e3643118bb
Matheus Reis <matheus.r.araujo11@aluno.senai.br>
2025-04-29 08:14:58 -0300

    Feat: Criando a component cadastrar

---
+++ filmes/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme';
+// import Login from './pages/login/Login';
+
+function App() {
+  return (
+    <>
+    {/* Chamar as paginas */}
+    {/* <Login/> */}
+    <CadastroFilme/>
+    </>
+  );
+}
+//Ele libera a função para ser usada
+export default App;
